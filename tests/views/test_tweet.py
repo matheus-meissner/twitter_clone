711fdf5ee219da7b1cbda3fb4db50bc008211482
= <=>
2024-06-25 11:23:43 -0300

    feat: Created a new view to separete it

--- tests/views/test_tweet.py
+++ tests/views/test_tweet.py
@@ -19,3 +19,9 @@ def test_relationship_view(client):
     url = reverse('followers')
     response = client.get(url)
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_relationship_view(client):
+    url = reverse('comments')
+    response = client.get(url)
+    assert response.status_code == 200
