a479c3c5c1fd273c87c4ca55cd6b9535b1c5cf07
Matheus Meissner <matheus.iembo@gmail.com>
2024-11-07 11:56:04 -0300

    cria_compatibilidade_com_vercel

--- social/settings.py
+++ social/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-skhzhc-#ts6w7!*$h@h5mboa)&ew@r#&g$xmd!f6ak8g_x0a)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.railway.app']
+ALLOWED_HOSTS = ['.vercel.app','.railway.app','localhost']
 
 
 # Application definition
