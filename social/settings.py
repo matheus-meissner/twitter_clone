ca6454254c32d0243e1416c70299840517f2e041
Matheus Meissner <matheus.iembo@gmail.com>
2024-11-12 18:03:30 -0300

    atualiza_settings

--- social/settings.py
+++ social/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'social.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Adicione este caminho para a pasta raiz
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
